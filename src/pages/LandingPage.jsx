c82c694108ed778978aab7462b6976bb5dc28445
Mikhael <goodboybutnot@gmail.com>
2022-08-11 14:17:27 +0700

    style(footer): rebuild footer
    rebuild the footer to make it responsive on any devices with some breakpoints
    
    Fixes #1

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function LandingPage() {
   return (
     <>
-      <div style={{ minHeight: 'inherit' }} />
-      <div style={{ minHeight: 'inherit' }} />
-      <div style={{ minHeight: 'inherit' }} />
-      <div style={{ minHeight: 'inherit' }} />
+      <div style={{ minHeight: 'inherit', backgroundColor: 'red' }} />
+      <div style={{ minHeight: 'inherit', backgroundColor: 'yellow' }} />
+      <div style={{ minHeight: 'inherit', backgroundColor: 'blue' }} />
+      <div style={{ minHeight: 'inherit', backgroundColor: 'orange' }} />
     </>
   );
 }
