685eefc489044788b90aafe11ee3b54b9a76b4b8
Mikhael <goodboybutnot@gmail.com>
2022-08-11 11:39:55 +0700

    style(navbar): rebuild navbar
    rebuild navbar to make it responsive on any devices
    
    Fixes #1

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import style from '../../styles/module-css/Navbar.module.css';
 
 function Navbar() {
   return (
-    <div className="nav">
-      <div className="nav-item">
-        <a href="/">Home</a>
-      </div>
-      <div className="nav-item">
-        <a href="/">Add Recipe</a>
-      </div>
-      <div className="nav-item">
-        <a href="/">Profile</a>
-      </div>
-      <div className="nav-item-auth">
-        <a href="/">Login</a>
+    <div className={style.navbar}>
+      <div className="row d-flex justify-content-center pt-lg-5 pt-md-5 m-0">
+        <div className="col-md-2 col-12 text-center">
+          <a href="/">Home</a>
+        </div>
+        <div className="col-md-2 col-12 text-center">
+          <a href="/">
+            Add Recipe
+          </a>
+        </div>
+        <div className="col-md-2 col-12 text-center">
+          <a href="/">
+            Profile
+          </a>
+        </div>
+        <div className="col-md-2 col-12 text-center ms-auto nav-auth-item">
+          <a href="/">
+            Login
+          </a>
+        </div>
       </div>
     </div>
   );
