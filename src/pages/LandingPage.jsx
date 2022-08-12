45fa69d2df7b44e8c5545650b29c5314964fc742
Mikhael <goodboybutnot@gmail.com>
2022-08-12 17:01:26 +0700

    style(landing page): landing page section two.
    create landing page section two, which is popular recipe part and create some mediaqueries for different breakpoints.
    
    init-section-two landing-page.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import LandingSectionOne from '../components/organism/LandingSectionOne';
-// import style from '../styles/module-css/LandingPage.module.css';
+import style from '../styles/module-css/LandingPage.module.css';
 
 function LandingPage() {
   return (
     <>
       <LandingSectionOne />
-      <div style={{ minHeight: 'inherit', backgroundColor: 'blue' }} />
+      <div className={style.sectionPageSize}>
+        <div className={style.sectionTitle}>
+          <span>Popular For You!</span>
+          <div className={style.titleLine} />
+        </div>
+        <div className={style.popularSection}>
+          <div className={style.popularCard} style={{ backgroundImage: "url('/images/pizza.svg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <span>Pizza Lamoa</span>
+          </div>
+          <div className={style.popularCard} style={{ backgroundImage: "url('/images/burger.svg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <span>King Burger</span>
+          </div>
+        </div>
+      </div>
+      {/* <div style={{ minHeight: 'inherit' }}>
+        New Recipe
+      </div> */}
     </>
 
   );
