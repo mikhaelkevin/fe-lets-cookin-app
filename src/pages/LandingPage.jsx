89d942254737a0f8be5c06841ece21979ed00395
Mikhael <goodboybutnot@gmail.com>
2022-08-13 22:40:32 +0700

    refactor(landing page): section four landing page.
    refactor section four into atomic design and integrate it with dummy data.
    
    refactor landing-page #4.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,9 +2,7 @@ import React, { useEffect } from 'react';
 import LandingSectionOne from '../components/organism/landing-page/LandingSectionOne';
 import LandingSectionTwo from '../components/organism/landing-page/LandingSectionTwo';
 import LandingSectionThree from '../components/organism/landing-page/LandingSectionThree';
-import style from '../styles/module-css/LandingPage.module.css';
-import SectionTitle from '../components/atomics/landing-page/SectionTitle';
-import { recipeListDummy } from '../utils/data';
+import LandingSectionFour from '../components/organism/landing-page/LandingSectionFour';
 
 function LandingPage() {
   useEffect(() => {
@@ -16,23 +14,7 @@ function LandingPage() {
       <LandingSectionOne />
       <LandingSectionTwo />
       <LandingSectionThree />
-      <div className={style.sectionPageSize}>
-        <SectionTitle data={{ title: 'Recipe list' }} />
-        <div className={style.recipeListContent}>
-          <div className="row recipeListLayouts">
-            {recipeListDummy?.map((item) => (
-              <div className="col-lg-4 col-md-6 col-12 mb-4" key={item?.id}>
-                <div className={style.cardCustomHeight}>
-                  <div className="card h-100 w-100">
-                    <span>{item?.title}</span>
-                    <img src={item?.image || '/images/broken-image.png'} alt="x" />
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <LandingSectionFour />
     </>
 
   );
