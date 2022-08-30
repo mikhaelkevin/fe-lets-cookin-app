c4a8bfb7a981f4bc62543a3dbd9d90553ccb8eb8
Mikhael <goodboybutnot@gmail.com>
2022-08-30 20:52:57 +0700

    style(auth): auth page init.
    style auth-features init.

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,29 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 // Components
-import Layouts from './components/global/Layouts';
-import LandingPage from './pages/LandingPage';
-import DetailRecipe from './pages/DetailRecipe';
-import Login from './pages/Login';
 import Auth from './pages/Outlet/Auth';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+
 import Content from './pages/Outlet/Content';
+import LandingPage from './pages/LandingPage';
+import DetailRecipe from './pages/Recipe/DetailRecipe';
 
 function App() {
   return (
     <BrowserRouter>
-      <Layouts>
-        <Routes>
-          <Route element={<Auth />}>
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
-        <Routes>
-          <Route element={<Content />}>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/detail" element={<DetailRecipe />} />
-          </Route>
-        </Routes>
-      </Layouts>
+      <Routes>
+        <Route element={<Auth />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+      </Routes>
+      <Routes>
+        <Route element={<Content />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/detail" element={<DetailRecipe />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/pages/Recipe/DetailRecipe.jsx
+++ src/pages/Recipe/DetailRecipe.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
-import style from '../styles/module-css/DetailResep.module.css';
-import { detailRecipeDummy } from '../utils/data';
+import style from '../../styles/module-css/DetailResep.module.css';
+import { detailRecipeDummy } from '../../utils/data';
 
-import HeadContent from '../components/organism/detail-recipe/HeadContent';
-import DescriptionContent from '../components/organism/detail-recipe/DescriptionContent';
-import CommentList from '../components/moleculs/detail-recipe/CommentList';
-import CommentForm from '../components/atomics/detail-recipe/CommentForm';
+import HeadContent from '../../components/organism/detail-recipe/HeadContent';
+import DescriptionContent from '../../components/organism/detail-recipe/DescriptionContent';
+import CommentList from '../../components/moleculs/detail-recipe/CommentList';
+import CommentForm from '../../components/atomics/detail-recipe/CommentForm';
 // import numberToWords from 'number-to-words';
 
 function DetailRecipe() {
